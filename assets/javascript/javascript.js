761ef26f224c607199cff65fe80316f61ae6e502
Samantha Olvera <smolvera2010@yahoo.com>
2018-03-24 13:59:15 -0400

    popped changes from stash-smo

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,17 +1,31 @@
+function getLyrics(){
+  var artistSearch = document.getElementById("artistSearch").value;
+  document.getElementById("lyrics").textContent = "";
+  $.ajax({
+    type: "Get",
+    data: {
+      
+      apikey: "32aebbe7388e91bf330ba15b45c9c70e",
+      q_artist: artistSearch,
 
-// Get the video
-var video = document.getElementById("myVideo");
+      format:"jsonp",
+      
+      callback: "jasonp_callback"
 
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-    if (video.paused) {
-        video.play();
-        btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        btn.innerHTML = "Play";
-    }
+    },
+    url: "http://api.musixmatch.com/ws/1.1/track.search",
+    dataType: "jsonp",
+    jsonpCallback: 'jsonp_callback',
+    contentType: 'application/json',
+    success: function(data) {
+        console.log(data); 
+        console.log(data.message.body.track_list[0].track.album_coverart_350x350);
+  }})
 }
+
+getLyrics();
+
+
+  
+  
+
